3481cf4aa0986cf8aff6d03629e430b3b62ad3db
anthony29p <anthony.perez@uni.pe>
2022-09-11 16:19:16 -0500

    Back almost finished

--- Api/src/app.js
+++ Api/src/app.js
@@ -1,9 +1,12 @@
 // Server configuration
 
 const express = require('express')
+const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index.js');
+// const multer = require('multer') // v1.0.5
+// const upload = multer()
 
 const app = express()
 
@@ -13,8 +16,10 @@ app.use(morgan('dev'))
 
 app.use('/', routes);
 
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use(cookieParser());
 
 
 module.exports = app;
--- Api/src/controllers/index.js
+++ Api/src/controllers/index.js
@@ -1,21 +1,39 @@
 const {pool} = require('../database.js');
 
 const getProducts = (req,res) =>{
-
-    pool.query('SELECT * FROM product', function (error, results) {
-        if (error) console.log(error);
-        res.status(200).json(results);
-    })
-
+    const { name } = req.query
+    try{
+        pool.getConnection(function(err,conn){
+            conn.query('SELECT * FROM product', function (error, results) {
+                if(name){
+                const filteredProducts = results.filter(r => r.name.toLowerCase().includes(name.toLowerCase()))
+                res.status(200).json(filteredProducts)
+                conn.release()
+                }
+                else{
+                res.status(200).json(results)
+                conn.release()
+                }
+            })
+        })
+    }
+    catch(error){
+        res.status(400).send('Error ocurred at product')
+    }
 }
 
 const getCategories = (req,res) =>{
-
-    pool.query('SELECT * FROM category', function (error, results) {
-        if (error) console.log(error);
-        res.status(200).json(results);
-    })
-    
+    try{
+        pool.getConnection(function(error1,conn){
+            conn.query('SELECT * FROM category', function (error2, results) {
+                res.status(200).json(results)
+                conn.release()
+            })
+        })
+    }
+    catch(error){
+        res.status(400).send('Error ocurred at category')
+    }
 }
 
 module.exports = {
@@ -23,3 +41,21 @@ module.exports = {
     getCategories,
 
 }
+
+//other syntax for getting products
+
+// pool.getConnection(function(err,conn){
+//     if(err){res.send('Error ocurred')
+//     }
+//     else{
+//         conn.query('SELECT * FROM product', function (error, results) {
+//             if (!error){
+//             res.status(200).json(results)};
+//             conn.release();
+//         })
+//     }
+// })
+
+// pool.query('SELECT * FROM product', function (error, results) {
+//     if (!error) res.status(200).json(results);
+// })
